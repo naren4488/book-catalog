732408a23c6462902cf2cec64f97a16b1e94acd7
naren4488 <narendrakajla77@gmail.com>
2024-04-05 14:38:29 +0530

    try-exception

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
 
+
+# if __name__ == '__main__':
+#     app = create_app('dev')
+#     with app.app_context():
+#         db.create_all()
+#         if not User.query.filter_by(user_name='harry').first():
+#             User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
+#     app.run(port=8000)
+
+
 app = create_app('prod')
 with app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
-        # app.run()
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
+    except Exception as e:
+        print(e)
