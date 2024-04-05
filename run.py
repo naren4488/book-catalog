04ed28447e9c47e3129d74420133788d6db5bf48
naren4488 <narendrakajla77@gmail.com>
2024-04-05 14:15:42 +0530

    procfile

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
 
-flask_app = create_app('prod')
-with flask_app.app_context():
+app = create_app('prod')
+with app.app_context():
     db.create_all()
     if not User.query.filter_by(user_name='harry').first():
         User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
-        flask_app.run()
+        # app.run()
