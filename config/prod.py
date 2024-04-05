fba955051b05cada96d367e1a47115415945a866
naren4488 <narendrakajla77@gmail.com>
2024-04-05 11:55:20 +0530

    hosting configurations

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,7 @@
+import os
+
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:narendra@localhost:5432/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
